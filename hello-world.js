94367db007e9083dd64da7606b55488e8285a1c4
prajakta <83332545+prajakta2594@users.noreply.github.com>
2021-06-08 20:38:21 +0530

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 80;
+var port = 7070;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
